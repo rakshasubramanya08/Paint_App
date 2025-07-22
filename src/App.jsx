435f0f9d64830924e62731829efbba6d6d781bcc
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-07-22 17:05:12 +0200

    added another div tag to add label and input type into it.

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ function App(){
       <h1 className="word"> PAINT APP </h1>
     
     <div className="container">
-     
+      <div className='box'>
+        <label for = "col" className='col1'>color: </label>
+        <input type="color" className="color" name='col'/>
 
+        <label for = "range1">Brush Width: </label>
+        <input type="range" className='range1' name="range1"></input>
+
+         <label for = "range2">Brush Opacity: </label>
+        <input type="range" className='range2' name="range2"></input>
+      </div> 
     </div>
-
-</div>
-   
-    
-
-
-
-
-
+    </div>
     </>
   )
 }
